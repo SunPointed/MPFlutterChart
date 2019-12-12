c899614a45f9a4e4ff26ae686c9235cd552f0425
罗秋雨 <410338643@qq.com>
2019-12-12 11:10:24 +0800

    change

--- mp_chart/upload.sh
+++ mp_chart/upload.sh
@@ -1,7 +1,7 @@
 sudo /usr/local/sbin/privoxy /usr/local/etc/privoxy/config
 export http_proxy='http://localhost:8118'
 export https_proxy='http://localhost:8118'
-pub publish --server=https://pub.dartlang.orgy
+pub publish --server=https://pub.dartlang.org
 unset http_proxy
 unset https_proxy
 sudo service privoxy stop
