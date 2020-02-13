d1fc6544d73fca5520740bd82abad0e71bba5c10
Komang Sidhi Artha <sidhi@jakpat.net>
2020-02-13 14:03:02 +0700

    Pie Chart Configuration:
    Enable Ability to change Pie Chart, Doughnut type, Hole Color

--- mp_chart/lib/mp/core/render/pie_chart_renderer.dart
+++ mp_chart/lib/mp/core/render/pie_chart_renderer.dart
@@ -228,7 +228,7 @@ class PieChartRenderer extends DataRenderer {
       }
     }
 
-    renderPaint..color = ColorUtils.WHITE;
+    renderPaint..color = _painter.getHoleColor();
     c.drawCircle(Offset(center.x, center.y), radius, renderPaint);
 
     final double sliceSpace =
@@ -405,7 +405,7 @@ class PieChartRenderer extends DataRenderer {
       angle += sliceAngle * phaseX;
     }
 
-    renderPaint..color = ColorUtils.WHITE;
+    renderPaint..color = _painter.getHoleColor();
     c.drawCircle(
         Offset(center.x, center.y), mInnerRectBuffer.width / 2, renderPaint);
 
@@ -1021,7 +1021,7 @@ class PieChartRenderer extends DataRenderer {
 
 //      mBitmapCanvas.drawPath(mPathBuffer, renderPaint);
       c.drawPath(mPathBuffer, renderPaint);
-      renderPaint..color = ColorUtils.WHITE;
+      renderPaint..color = _painter.getHoleColor();
       c.drawOval(mInnerRectBuffer, renderPaint);
     }
 
