f437182d14f9bb9c0497fd196b01f0b2ffe0e7d8
Haemavitch Varith <haemavitch@gmail.com>
2021-04-30 22:39:51 +0700

    Updated dependency versions

--- mp_chart/lib/mp/chart/chart.dart
+++ mp_chart/lib/mp/chart/chart.dart
@@ -46,15 +46,7 @@ abstract class ChartState<T extends Chart> extends State<T> {
 
     String fileName = DateTime.now().toIso8601String();
     String path = '$directory/$fileName.png';
-    _screenshotController.capture(path: path, pixelRatio: 3.0).then((imgFile) {
-      ImageGallerySaver.saveImage(Uint8List.fromList(imgFile.readAsBytesSync()))
-          .then((value) {
-        imgFile.delete();
-      });
-      isCapturing = false;
-    }).catchError((error) {
-      isCapturing = false;
-    });
+    _screenshotController.captureAndSave(path);
   }
 
   @override
